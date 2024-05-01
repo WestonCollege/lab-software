9d69ae137431fd8c7b9ea32794feeda6d960ef0f
Jason Hill <jasonhill2000@users.noreply.github.com>
2024-05-01 12:02:16 +0100

    Update lab.ps1

--- lab.ps1
+++ lab.ps1
@@ -1,10 +1,8 @@
+#install hyper-v
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine
 
-
-
-
-
+##install choco
 Set-ExecutionPolicy AllSigned
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
